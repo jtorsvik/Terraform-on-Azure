ea9ea272a2e37938eda0bff8cc64ebfaf85f2b26
Joakim M Torsvik <joakim.torsvik@icloud.com>
2024-12-06 12:42:59 +0100

    fix: Removed tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "3a01d291-8916-445e-be20-03082f6b8cc0"
-  tenant_id       = "c6946a08-ad72-4542-a1c1-d3ee4a5ec87a"
-  client_id       = "4614511b-7496-47d1-b14f-3852f70ac7bf"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   features {}
 }
